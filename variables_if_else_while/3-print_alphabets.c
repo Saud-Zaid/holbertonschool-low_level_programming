f2df80eb3ff9b353f776d685c7861557a77f78ff
Saud_Alhammadi <saud.zaid.442@gmail.com>
2026-07-11 15:17:15 +0300

    Add task 3 print alphabets lowercase and uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, and then in uppercase.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	ch = 'A';
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
