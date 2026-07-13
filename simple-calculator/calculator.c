02ebda950b8eea6a00d39e958f17f1271611207d
Saud_Alhammadi <saud.zaid.442@gmail.com>
2026-07-13 18:06:54 +0300

    Task 2: Implement addition end-to-end

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
 /**
+ * add - Adds two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
  * main - Entry point for the simple calculator program
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int choice;
+	int a, b, result;
 
 	do {
 		printf("Simple Calculator\n");
@@ -15,9 +27,9 @@ int main(void)
 		printf("3) Multiply\n");
 		printf("4) Divide\n");
 		printf("0) Quit\nChoice: ");
-
+		
 		scanf("%d", &choice);
-
+		
 		if (choice == 0)
 		{
 			printf("Bye!\n");
@@ -26,6 +38,26 @@ int main(void)
 		{
 			printf("Invalid choice\n");
 		}
+		else
+		{
+			/* ننفذ العمليات فقط إذا كان الخيار صحيحاً بين 1 و 4 */
+			switch (choice)
+			{
+			case 1:
+				printf("A: ");
+				scanf("%d", &a);
+				printf("B: ");
+				scanf("%d", &b);
+				
+				result = add(a, b);
+				printf("Result: %d\n", result);
+				break;
+			
+			default:
+				break;
+			}
+		}
+		
 	} while (choice != 0);
 
 	return (0);
