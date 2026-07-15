fa5a3377f29ca5b7aa026946c77b61e943695c02
Saud_Alhammadi <saud.zaid.442@gmail.com>
2026-07-15 18:15:16 +0300

    Solve task 0: _strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	/* الوصول لنهاية النص الأول */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	/* نسخ النص الثاني إلى نهاية الأول */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
