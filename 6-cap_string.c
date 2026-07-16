c54d99594607e1d26f37f5c58f718ae15578ec32
Saud_Alhammadi <saud.zaid.442@gmail.com>
2026-07-16 18:09:54 +0300

    Solve task 6: cap_string

---
+++ 6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to modify
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	/* تكبير الحرف الأول في السلسلة إذا كان صغيراً */
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] -= 32;
+	}
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			/* إذا وجدنا فاصل والحرف التالي صغير */
+			if (str[i] == sep[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
