7484d3d7806af30a3392d7d0f594c0cfbb741ca9
Saud_Alhammadi <saud.zaid.442@gmail.com>
2026-07-23 18:04:33 +0300

    Solve Task 2: Concatenate two strings using malloc

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to newly allocated space containing s1 + s2,
+ * or NULL on failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	unsigned int len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+
+	while (s2[len2] != '\0')
+		len2++;
+
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat == NULL)
+		return (NULL);
+
+	while (i < len1)
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+
+	while (j < len2)
+	{
+		concat[i + j] = s2[j];
+		j++;
+	}
+
+	concat[i + j] = '\0';
+
+	return (concat);
+}
