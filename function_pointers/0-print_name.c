fcc9544746ce630bc4e2a2a0db006db9c6660d77
Saud_Alhammadi <saud.zaid.442@gmail.com>
2026-07-30 19:35:23 +0300

    0. What's my name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.size_t>
+
+/**
+ * print_name - Prints a name using a given function pointer.
+ * @name: Name of the person to print.
+ * @f: Pointer to the function used to print the name.
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
