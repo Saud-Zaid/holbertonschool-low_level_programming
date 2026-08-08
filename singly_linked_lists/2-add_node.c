496b39aab16f539ab70f1cb386c5556c7c5c7baf
Saud_Alhammadi <saud.zaid.442@gmail.com>
2026-08-08 22:39:31 +0300

    Fix lists.h prototype and complete Task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -5,7 +5,7 @@
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: double pointer to the list_t list
- * @str: string to be duplicated and added to the new node
+ * @str: string to be duplicated and added
  *
  * Return: the address of the new element, or NULL if it failed
  */
